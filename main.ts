42132caf28276b401c83c25f0a41b650f12c996d
Erica710 <116471873+Erica710@users.noreply.github.com>
2022-10-23 10:54:52 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
+let _1 = 0
 basic.forever(function () {
-    let _1 = 0
     if (input.lightLevel() > 30) {
-    	
+        _1 = randint(1, 3)
+        if (_1 == 1) {
+            basic.showIcon(IconNames.Scissors)
+        } else if (_1 == 2) {
+            basic.showIcon(IconNames.Square)
+        } else {
+            basic.showIcon(IconNames.SmallSquare)
+        }
     }
-    if (_1 == 1) {
-        basic.showIcon(IconNames.Scissors)
-    } else if (_1 == 2) {
-        basic.showIcon(IconNames.SmallSquare)
-    } else {
-        basic.showIcon(IconNames.Square)
+})
+basic.forever(function () {
+    if (input.lightLevel() < 30) {
+        basic.clearScreen()
     }
 })
